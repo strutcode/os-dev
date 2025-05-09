94136d3493c3aad19e1ff805edd4f1d09f9fc9cb
Tristan Shelton <tristan.m.shelton@gmail.com>
2025-05-08 17:30:52 -0700

    Hello world bootloader

--- src/boot.asm
+++ src/boot.asm
@@ -1,9 +1,30 @@
-; Infinite loop (e9 fd ff)
-loop:
-    jmp loop 
+[org 0x7C00]			; Set the origin - standard for bootloaders
 
-; Fill with 510 zeros minus the size of the previous code
-times 510-($-$$) db 0
-; Magic number
-dw 0xaa55 
+jmp start			; Jump to start of code
 
+section .text
+msg     db  "Hello World!",10	; Our message with a newline
+len     equ $ - msg		; Calculate length of message
+
+section .text
+start:
+	mov	ah, 0x0e	; tty mode
+
+	mov	edx, len	; Calculate message length
+	mov	ecx, msg	; Load address of message
+
+print:
+	mov	al, [ecx]	; Load current character
+	int	0x10		; Print character
+	inc	ecx		; Move to next character
+	dec	edx		; Decrement length
+	jnz	print		; If length is not zero, continue printing
+
+	hlt			; Halt the CPU
+	jmp 	$		; If the previous failed, start an infinite loop
+
+; Zeros up to byte 510
+times 510 - ($ - $$) db 0
+
+; Boot signature at bytes 511-512
+dw 0xaa55
